fa09ff917a06bbfbdae57f769b0450e8da77bf35
karlafelix <karlakaroline_7@hotmail.com>
2017-06-12 16:59:22 -0400

    Boss[2]

--- Assets/chefe.cs
+++ Assets/chefe.cs
@@ -2,33 +2,124 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class chefe : MonoBehaviour {
-    private Transform target;
-    Transform Way;
-    private Animator animator;
-    private float Distancia;
-    public int vidaBoss;//vida do sapo
-    private GameObject Dino;
-    public int Damage;//dano do ataque do sapo
-    private bool CanMove;
-    public float vel;
-    Rigidbody2D rb;
-    private bool podeReceber;
+public class move: MonoBehaviour {
 
-    // Use this for initialization
+    private Transform Boss;
+    public waypoint[] wayPoints;
+    public float speed = 3f;
+    public bool isCircular;
+    public bool inReverse = true;
+    private waypoint currentWaypoint;
+    private int currentIndex = 0;
+    private bool isWaiting = false;
+    private float speedStorage = 0;
+
+
     void Start () {
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        Way = GameObject.FindGameObjectWithTag("waypoint").transform;//player
-        CanMove = true;
-        podeReceber = true;
-        animator = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Animator>();
 
+
+        Boss = gameObject.GetComponent<Transform>();
+
+        if (wayPoints.Length > 0)
+        {
+            currentWaypoint = wayPoints[0];
+        }
     }
+
 	
-	// Update is called once per frame
-	void Update () {
-        if (CanMove)
-            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, Way.position, 2 * Time.deltaTime);
-
+	void Update ()
+    {
+        if (currentWaypoint != null && !isWaiting)
+        {
+            MoveTowardsWaypoint();
+        }
     }
+
+    void Pause()
+    {
+        isWaiting = !isWaiting;
+    }
+
+    private void MoveTowardsWaypoint()
+    {
+        // Get the moving objects current position
+        Vector3 currentPosition = this.transform.position;
+
+        // Get the target waypoints position
+        Vector3 targetPosition = currentWaypoint.transform.position;
+
+        // If the moving object isn't that close to the waypoint
+        if (Vector3.Distance(currentPosition, targetPosition) > .1f)
+        {
+
+            // Get the direction and normalize
+            Vector3 directionOfTravel = targetPosition - currentPosition;
+            directionOfTravel.Normalize();
+
+            //scale the movement on each axis by the directionOfTravel vector components
+            this.transform.Translate(
+                directionOfTravel.x * speed * Time.deltaTime,
+                directionOfTravel.y * speed * Time.deltaTime,
+                directionOfTravel.z * speed * Time.deltaTime,
+                Space.World
+            );
+        }
+        else
+        {
+
+            // If the waypoint has a pause amount then wait a bit
+            if (currentWaypoint.waitSeconds > 0)
+            {
+                Pause();
+                Invoke("Pause", currentWaypoint.waitSeconds);
+            }
+
+            // If the current waypoint has a speed change then change to it
+            if (currentWaypoint.speedOut > 0)
+            {
+                speedStorage = speed;
+                speed = currentWaypoint.speedOut;
+            }
+            else if (speedStorage != 0)
+            {
+                speed = speedStorage;
+                speedStorage = 0;
+            }
+
+            NextWaypoint();
+        }
+    }
+
+    private void NextWaypoint()
+    {
+        if (isCircular)
+        {
+
+            if (!inReverse)
+            {
+                currentIndex = (currentIndex + 1 >= wayPoints.Length) ? 0 : currentIndex + 1;
+            }
+            else
+            {
+                currentIndex = (currentIndex == 0) ? wayPoints.Length - 1 : currentIndex - 1;
+            }
+
+        }
+        else
+        {
+
+            // If at the start or the end then reverse
+            if ((!inReverse && currentIndex + 1 >= wayPoints.Length) || (inReverse && currentIndex == 0))
+            {
+                inReverse = !inReverse;
+            }
+            currentIndex = (!inReverse) ? currentIndex + 1 : currentIndex - 1;
+
+        }
+
+        currentWaypoint = wayPoints[currentIndex];
+    }
+
 }
+
+
