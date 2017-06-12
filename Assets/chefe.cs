596fc0a2ad4be3c5f4526a3454c2b79092198e95
karlafelix <karlakaroline_7@hotmail.com>
2017-06-12 02:17:02 -0400

    waypoints

--- Assets/chefe.cs
+++ Assets/chefe.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class chefe : MonoBehaviour {
+    private Transform target;
+    private Animator animator;
+    private float Distancia;
+    public int vidaBoss;//vida do sapo
+    private GameObject Dino;
+    public int Damage;//dano do ataque do sapo
+    private bool CanMove;
+    public float vel;
+    Rigidbody2D rb;
+    private bool podeReceber;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        Way = GameObject.FindGameObjectWithTag("waypoint").transform;//player
+        CanMove = true;
+        podeReceber = true;
+        animator = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Animator>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (CanMove)
+            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, Way, 2 * Time.deltaTime);
+
+    }
 }
